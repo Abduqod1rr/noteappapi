3076dc74d4cd9971de04fa48df2c4bf466ca75d9
Abduqod1rr <ttheonepersonn@gmail.com>
2025-10-04 21:56:01 +0500

    .

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Note
 
-class noteserializers(serializers.Serializer):
+class noteserializers(serializers.ModelSerializer):
     class Meta:
         model=Note
         fields=['id','title','text']
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Note
+from .serializer import noteserializers
+from rest_framework import generics
+from .permissions import IsOwner
 # Create your views here.
+
+class WriteNote(generics.CreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class=noteserializers
+    
+
+class CrudNote(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class=noteserializers
+    permission_classes=[IsOwner]
