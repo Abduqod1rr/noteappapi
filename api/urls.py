f71b992d65b0414f02463caf200d1ba7b6c9f06e
Abduqod1rr <ttheonepersonn@gmail.com>
2025-10-04 21:57:47 +0500

    .

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path ,include
+from . import views
 
 urlpatterns = [
     path('',include('rest_framework.urls')),
+    path("writenote/", views.WriteNote.as_view(), name="writenote"),
+    path("crud-note/", views.CrudNote.as_view(), name="crud-note")
 
 ]
